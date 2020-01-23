8d38e81e4ca6003df826c8f232b27c4b7b80ec59
qsbcn12 <quirine.smit@zuidema.nl>
2020-01-23 16:55:10 +0100

    Created clean_sentiment_lexicon.py and got it working for English

--- clean_sentiment_lexicon.py
+++ clean_sentiment_lexicon.py
@@ -2,13 +2,38 @@
 """
 Spyder Editor
 """
+from collections import defaultdict
 
-filepath_1 = "./En-sent-lex/NRC-Affect-Intensity-Lexicon/NRC-AffectIntensity-Lexicon.txt"
-filepath_2 = './En-sent-lex/AutomaticallyGeneratedLexicons/NRC-Hashtag-Emotion-Lexicon-v0.2/NRC-Hashtag-Emotion-Lexicon-v0.2.txt'
-filepath_3 = './En-sent-lex/AutomaticallyGeneratedLexicons/NRC-Hashtag-Sentiment-Lexicon-v1.0/HS-unigrams.txt'
-filepath_4 = './En-sent-lex/NRC-Emotion-Lexicon-v0.92/NRC-Emotion-Lexicon-Wordlevel-v0.92.txt'
+filepath = './lexicon/English/lexicon_meta_data_English.txt'
 
-def read_term_score_emotion(filename, word_index=0, score_index=1, emotion_index=-1):
+def read_meta_file(filepath):
+    meta_data = []
+    with open(filepath, 'r', encoding='utf-8') as infile:
+        for line in infile:
+            # Remove \n at end of line
+            if line.endswith('\n'):
+                line = line[:-1]
+            # Skip header line
+            if line.startswith('index'):
+                continue
+            # Split lines on tab
+            split_line = line.split("\t")
+            # Change indexes from string to integer type
+            split_line[2] = int(split_line[2])
+            split_line[3] = int(split_line[3])
+            split_line[4] = int(split_line[4])
+            meta_data.append(split_line)
+    return meta_data
+            
+def term_score_emotion_to_dict(meta_data_list):
+    # Get data from meta_data_list
+    index = meta_data_list[0]
+    filename = meta_data_list[1]
+    word_index = meta_data_list[2] 
+    score_index = meta_data_list[3]
+    emotion_index = meta_data_list[4]
+
+    # Set dictionary
     word_dict = dict() # dict of dicts; {word1: {emotion1:score}, word2: {emotion1:score}}
     with open(filename, 'r', encoding='utf-8') as infile:
         for line in infile:
@@ -23,27 +48,57 @@ def read_term_score_emotion(filename, word_index=0, score_index=1, emotion_index
             # Set variables
             word = split_line[word_index]
             score = split_line[score_index]
-            # If emotion index is given, have the emotion be the key of the dictionary for each word
-            # {word1: {emotion1:score}, word2: {emotion1:score}}
+            # If emotion index is given, the emotion + the index corresponding to the file
+            # is the key of the dictionary for each word
             if emotion_index != -1:
-                emotion = split_line[emotion_index]
-                if word in word_dict:
-                    word_dict[word][emotion] = score
-                else:
-                    word_dict[word] = dict()
-                    word_dict[word][emotion] = score
-            # If there is no emotion specified, then do not include key=word and value=score
-            # {word1: score, word2: score}
-            if emotion_index == -1:
-                word_dict[word] = score
+                emotion = split_line[emotion_index] + index
+            # If the emotion is not given then the emotion name is replaced by 'score'
+            elif emotion_index == -1:
+                emotion = 'score' + index
+            # {word1: {emotion1:score, emotion2:score}, word2: {emotion1:score}}
+            if word in word_dict:
+                word_dict[word][emotion] = score
+            else:
+                word_dict[word] = dict()
+                word_dict[word][emotion] = score
     return word_dict
  
-#print(read_term_score_emotion(filepath, word_index=0, score_index=1, emotion_index=2))
-#print(read_term_score_emotion(filepath_2, word_index=1, score_index=2, emotion_index=0))
-#print(read_term_score_emotion(filepath_3, word_index=0, score_index=1))
-#print(read_term_score_emotion(filepath_4, word_index=0, score_index=2, emotion_index=1))   
-'''
-to do:
-    write to jsons
-    Figure out how to combine data
-'''
+def combine_dicts(list_of_dicts):
+    main_dict = dict()
+    for lexicon_dict in list_of_dicts:
+        for word, inner_dict in lexicon_dict.items():
+            if word not in main_dict:
+                main_dict[word] = dict([('positive3', 0), ('score2', 0), 
+                         ('fear3', 0), ('AffectDimension0', 0), ('fear0', 0), 
+                         ('surprise3', 0), ('surprise1', 0), ('anger0', 0), 
+                         ('anticipation1', 0), ('sadness3', 0), ('disgust3', 0), 
+                         ('anger3', 0), ('joy3', 0), ('negative3', 0), 
+                         ('anticipation3', 0)])
+            for emotion, score in inner_dict.items():
+                main_dict[word][emotion] = score
+    
+    return main_dict
+    
+    
+def inport_sentiment_lexicons(meta_filepath):
+    '''
+    Takes file path with meta data of lexicons and their file paths.
+    Reads the lexicons (with word, emotion and score)
+    Creates a combined dictionary looking as following:
+        {word1: {'positive3': '0', 'score2': '-0.577', 'fear3': '1', 
+        'AffectDimension0': 0, 'fear0': '0.828', 'surprise3': '0', ...},
+        word2: {'positive3': '1', 'score2': '0.4', 'fear3': '1', 
+        'AffectDimension0': 1, 'fear0': '0.3', 'surprise3': '0', ...},...}
+    Returns this dictionary of dictionaries.        
+    '''
+    meta_data_list = read_meta_file(filepath)
+    list_of_lexicon_dicts = []
+    for lexicon_meta_data in meta_data_list:
+        lexicon_dict = term_score_emotion_to_dict(lexicon_meta_data)
+        list_of_lexicon_dicts.append(lexicon_dict)
+    
+    main_dict = combine_dicts(list_of_lexicon_dicts)
+    return main_dict
+        
+    
+    
